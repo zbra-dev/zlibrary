4f1d1c5dafa054b0d4eb15b45e36c09e7cdfa4c0
Jayme Preto <jayme-arthur@hotmail.com>
2018-05-16 14:35:02 -0300

    - Fixed get jpg image when user have slack avatar

--- server/ZLibrary.Web/Controllers/Items/SlackUserDTO.cs
+++ server/ZLibrary.Web/Controllers/Items/SlackUserDTO.cs
@@ -4,18 +4,19 @@ using System.Runtime.Serialization;
 
 namespace ZLibrary.Web.Controllers.Items
 {
-    [DataContract (Name = "SlackUserDTO")]
+    [DataContract(Name = "SlackUserDTO")]
     public class SlackUserDTO
     {
         private const string ImageProtocol = "https";
-        private const string ImageExtension = ".png";
+        private const string PNGExtension = ".png";
+        private const string JPGExtension = ".jpg";
 
         [DataMember(Name = "ok")]
         public string Ok { get; set; }
 
         [DataMember(Name = "scope")]
         public string Scope { get; set; }
-        
+
         [DataMember(Name = "access_token")]
         public string AccessToken { get; set; }
 
@@ -30,8 +31,12 @@ namespace ZLibrary.Web.Controllers.Items
             if (!string.IsNullOrEmpty(User.UserAvatarUrl))
             {
                 var imageURLs = User.UserAvatarUrl.Split(ImageProtocol);
-                var imageUrl = imageURLs.FirstOrDefault(url => url.Contains(ImageExtension));
-                if (!string.IsNullOrEmpty(imageUrl)) 
+                var imageUrl = imageURLs.FirstOrDefault(url => url.Contains(PNGExtension));
+                if (string.IsNullOrEmpty(imageUrl))
+                {
+                    imageUrl = imageURLs.FirstOrDefault(url => url.Contains(JPGExtension));
+                }
+                if (!string.IsNullOrEmpty(imageUrl))
                 {
                     return string.Concat(ImageProtocol, Uri.UnescapeDataString(imageUrl));
                 }
